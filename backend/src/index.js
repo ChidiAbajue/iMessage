0822492d89922e5d80558712c2f7d45df7d8e1dd
ChidiAbajue <benedictabajue@gmail.com>
2026-09-14 07:16:01 +0100

    Add Dockerfile and .dockerignore; implement Express server static file serving

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,10 +4,12 @@ import "dotenv/config"
 import User from "./models/user.model.js"
 import {connectDB} from "./lib/db.js"
 import {clerkMiddleware} from "@clerk/express"
-
+import path from "path"
+import fs from "fs"
 const app = express()
 const PORT = process.env.PORT 
 const FRONTEND_URL = process.env.FRONTEND_URL
+const publicDir = path.join(process.cwd(), "public")
 
 app.use(express.json())
 app.use(cors({origin:FRONTEND_URL, credentials:true}))
@@ -16,6 +18,14 @@ app.use(clerkMiddleware())
 app.get("/health", (req, res) => {
     res.status(200).json({ok:true})
 })
+
+if (fs.existsSync(publiicDir)){
+    app.use(express.static(publicDir))
+    app.get("/{*any}", (req, res, next) => {
+      res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+    })
+}
+
 app.listen(3000, () => {
     connectDB()
     console.log('Server is up and running on Port', PORT || 3000)
