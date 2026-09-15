13efaeb901927c0dcfc7f15c0f256f8bf6be9afd
ChidiAbajue <benedictabajue@gmail.com>
2026-09-15 07:18:59 +0100

    feat: implement authentication UI components and loading states

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,19 +6,19 @@ import { WallpaperProvider } from './context/WallpaperContext';
 import { Navigate, Route, Routes } from 'react-router';
 import ChatPage from './pages/ChatPage';
 import AuthPage from './pages/AuthPage';
-
+import PageLoader from './components/PageLoader.jsx'
 
 
 function App() {
   const {isSignedIn, isLoaded} = useAuth()
   //todo: make this a better component
-  if (!isLoaded) return <p>Loading...</p>
+  if (!isLoaded) return <PageLoader />
   return (
     <ThemeProvider>
       <WallpaperProvider>
         <Routes>
           <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace/> }></Route>
-          <Route path="/auth" element={!isSignedIn ? <AuthPage /> : <Navigate to={"/chat"} replace/>}></Route>
+          <Route path="/auth" element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace/>}></Route>
         </Routes>
       </WallpaperProvider>
     </ThemeProvider>
