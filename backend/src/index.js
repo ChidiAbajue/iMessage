fcfe786235493c5590534bf72a0461faff70815a
ChidiAbajue <benedictabajue@gmail.com>
2026-09-15 08:29:13 +0100

    fix: remove unused app export and improve static file handling

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ import job from "./lib/cron.js";
 import clerkWebhook from "./webhooks/clerk.webhook.js"
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
-import { app, server } from "./lib/socket.js"
+import { server } from "./lib/socket.js"
 
 const app = express()
 const PORT = process.env.PORT 
@@ -32,10 +32,13 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
 
-if (fs.existsSync(publicDir)){
+if (fs.existsSync(publicDir)) {
     app.use(express.static(publicDir))
-    app.get("/{*any}", (req, res, next) => {
-      res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+
+    app.get(/^\/(?!api).*/, (req, res, next) => {
+      res.sendFile(path.join(publicDir, "index.html"), (err) => {
+        if (err) next(err)
+      })
     })
 }
 
